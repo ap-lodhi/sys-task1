d8898bf5af33e1f59eadf515a6e66428fd835ba4
Anil patel <87072168+ap-lodhi@users.noreply.github.com>
2023-09-08 16:32:14 +0530

    Update product.js

--- Task-1/jsFiles/product.js
+++ Task-1/jsFiles/product.js
@@ -1,10 +1,10 @@
 var userArray = JSON.parse(localStorage.getItem('userData')) || [];
 
-// Check if userArray has a username
+
 if (userArray.username) {
     var usernameElement = document.getElementById('username');
     usernameElement.textContent = userArray.username;
 } else {
-    // Handle the case where the username is not available
+  
     console.log("Username not found in userArray.");
 }
